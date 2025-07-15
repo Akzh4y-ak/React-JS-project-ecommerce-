af21ebd5ba7c8ec0e33d75209c0769d32c432dad
AkshayKannan <iamakshy81@gmail.com>
2025-07-15 15:28:15 +0530

    added service js data and started to create the service cards

---
+++ src/productData/serviceData.js
@@ -0,0 +1,27 @@
+// serviceData.js
+export const serviceData = [
+  {
+    icon: "car",
+    title: "Free Shipping",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#fdefe6",
+  },
+  {
+    icon: "card",
+    title: "Safe Payment",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#ceebe9",
+  },
+  {
+    icon: "shield-half-outline",
+    title: "Secure Payment",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#e2f2b2",
+  },
+  {
+    icon: "headset",
+    title: "Back Guarantee",
+    subtitle: "Lorem ipsum dolor sit amet.",
+    bg: "#d6e5fb",
+  },
+];
